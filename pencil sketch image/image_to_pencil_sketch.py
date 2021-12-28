ee8387cf6e362e385fd0d0b977fafb851c521c79
Google Vormodial <70081718+John-Divine@users.noreply.github.com>
2021-12-28 04:09:24 -0800

    converting an image to pencil sketch
    this is a project on how to convert an image into pencil sketch using cv2 in python

---
+++ pencil sketch image/image_to_pencil_sketch.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+
+'''Reading the image'''
+image = cv.imread('div.jpg')
+#cv.imshow('Div',image)
+
+'''Resizing image'''
+def resize(frame,scale=0.3):
+    width = int(frame.shape[1]*scale)
+    height = int(frame.shape[0]*scale)
+    dimension = (width,height)
+    return cv.resize(frame,dimension,interpolation=cv.INTER_AREA)
+image_resized = resize(image)
+#cv.imshow('Divine',image_resized)
+
+'''Processing image'''
+image_grayed = cv.cvtColor(image_resized,cv.COLOR_BGR2GRAY)
+#cv.imshow('grayed image',image_grayed)
+
+image_inverted = 255 - image_grayed
+#cv.imshow('inverted image',image_inverted)
+
+image_blured = cv.GaussianBlur(image_inverted,(21,21),0)
+#cv.imshow('blur',image_blured)
+blured_invert = 255 - image_blured
+pencil_scketch = cv.divide(image_grayed,image_blured,scale=256.0)
+
+'''Saving and displaying pencil image'''
+cv.imwrite('sketch.jpg',pencil_scketch)
+cv.imshow('sketch',pencil_scketch)
+
+cv.waitKey(0)
